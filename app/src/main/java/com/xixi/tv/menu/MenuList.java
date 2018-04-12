fb9296e5e5fb02fba77c06b10a77e9933a9dd5c1
SKY <357887966@qq.com>
2018-04-12 22:06:46 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/xixi/tv/menu/MenuList.java
@@ -0,0 +1,15 @@
+package com.xixi.tv.menu;
+
+/**
+ * Created by SKY on 2018/4/11.
+ */
+
+public class MenuList {
+    public static final String MENU_ITEM[] = {
+            "首页",
+            "电视剧",
+            "电影",
+            "综艺",
+            "动漫"
+    };
+}
